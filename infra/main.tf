72d71580afebb1a8f19e7ba87ae1e6d1bc93cbd4
Oussama Radouane <sofcasa1998@gmail.com>
2025-11-13 20:32:41 +0000

    switched server locally to docker

--- infra/main.tf
+++ infra/main.tf
@@ -1,90 +1,18 @@
-provider "docker" {}
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 
-# Create Docker network (so containers can talk to each other)
-resource "docker_network" "app_network" {
-  name = "react_app1_network"
+  backend "s3" {} # (later, optional)
 }
 
-# Create persistent volume for PostgreSQL
-resource "docker_volume" "pgdata" {
-  name = "ticket_pgdata"
-}
-
-# PostgreSQL Database container
-resource "docker_container" "ticket_db" {
-  name  = "ticket_db"
-  image = "postgres:15"
-
-  env = [
-    "POSTGRES_USER=postgres",
-    "POSTGRES_PASSWORD=postgres",
-    "POSTGRES_DB=ticketdb"
-  ]
-
-  
-  # Persist database data here
-  mounts {
-    target = "/var/lib/postgresql/data"
-    source = docker_volume.pgdata.name
-    type   = "volume"
-  }
-
-  # Optional: initialize schema only if volume is new
-  # Comment this out once DB is initialized
-  #mounts {
-  #  target = "/docker-entrypoint-initdb.d/init.sql"
-   # source = abspath("${path.module}/init.sql")
-   # type   = "bind"
-  #}
-
-  networks_advanced {
-    name = docker_network.app_network.name
-  }
-
-  ports {
-    internal = 5432
-    external = 5432
-  }
-}
-
-# Backend container (Node.js / Express)
-resource "docker_container" "backend" {
-  name  = "ticket_backend"
-  image = "react-app1-backend"
-
-  env = [
-    "DB_HOST=ticket_db",
-    "DB_USER=postgres",
-    "DB_PASSWORD=postgres",
-    "DB_NAME=ticketdb",
-    "DB_PORT=5432"
-  ]
-
-  networks_advanced {
-    name = docker_network.app_network.name
-  }
-
-  ports {
-    internal = 5000
-    external = 5000
-  }
-
-  depends_on = [docker_container.ticket_db]
-}
-
-# Frontend container (React)
-resource "docker_container" "frontend" {
-  name  = "ticket_frontend"
-  image = "react-app1-frontend"
-
-  networks_advanced {
-    name = docker_network.app_network.name
-  }
-
-  ports {
-    internal = 80
-    external = 3000
-  }
-
-  depends_on = [docker_container.backend]
-}
+# Cloud resources will go here later:
+#
+# - EC2 instance or ECS cluster
+# - Security groups
+# - Networking (VPC)
+# - Cloud PostgreSQL (Neon / RDS)
+# - Load balancer
